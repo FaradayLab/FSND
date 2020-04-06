c3d113c74d13b1b5f3a59cc012353645f6bd1128
FaradayLab <alexanderwilkins1@gmail.com>
2020-04-06 16:46:06 -0700

    package upate fix run error

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -12,18 +12,23 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+  db.app = app
+  db.init_app(app)
+  db.create_all()
+
 
 '''
 Question
 
 '''
-class Question(db.Model):  
+
+
+class Question(db.Model):
   __tablename__ = 'questions'
 
   id = Column(Integer, primary_key=True)
@@ -41,7 +46,7 @@ class Question(db.Model):
   def insert(self):
     db.session.add(self)
     db.session.commit()
-  
+
   def update(self):
     db.session.commit()
 
@@ -51,28 +56,28 @@ class Question(db.Model):
 
   def format(self):
     return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
+        'id': self.id,
+        'question': self.question,
+        'answer': self.answer,
+        'category': self.category,
+        'difficulty': self.difficulty,
     }
+
 
 '''
 Category
 
 '''
-class Category(db.Model):  
+
+
+class Category(db.Model):
   __tablename__ = 'categories'
 
   id = Column(Integer, primary_key=True)
   type = Column(String)
 
   def __init__(self, type):
-    self.type = type
+      self.type = type
 
   def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+      return {'id': self.id, 'type': self.type}
