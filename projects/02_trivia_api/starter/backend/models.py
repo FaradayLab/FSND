12ecdd60f71f11742224cd15915e527357d2e1b4
FaradayLab <alexanderwilkins1@gmail.com>
2020-04-08 14:01:15 -0700

    reformat

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -15,11 +15,11 @@ setup_db(app)
 
 
 def setup_db(app, database_path=database_path):
-  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-  db.app = app
-  db.init_app(app)
-  db.create_all()
+	app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	db.app = app
+	db.init_app(app)
+	db.create_all()
 
 
 '''
@@ -29,39 +29,39 @@ Question
 
 
 class Question(db.Model):
-  __tablename__ = 'questions'
+	__tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+	id = Column(Integer, primary_key=True)
+	question = Column(String)
+	answer = Column(String)
+	category = Column(String)
+	difficulty = Column(Integer)
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+	def __init__(self, question, answer, category, difficulty):
+		self.question = question
+		self.answer = answer
+		self.category = category
+		self.difficulty = difficulty
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
+	def insert(self):
+		db.session.add(self)
+		db.session.commit()
 
-  def update(self):
-    db.session.commit()
+	def update(self):
+		db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
 
-  def format(self):
-    return {
-        'id': self.id,
-        'question': self.question,
-        'answer': self.answer,
-        'category': self.category,
-        'difficulty': self.difficulty,
-    }
+	def format(self):
+		return {
+			'id': self.id,
+			'question': self.question,
+			'answer': self.answer,
+			'category': self.category,
+			'difficulty': self.difficulty,
+		}
 
 
 '''
